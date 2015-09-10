93bea7e9b05b4b3f72d717234fd58c60c5daf5c0
Seth Tippetts <sethtippetts@gmail.com>
2015-09-09 23:02:36 -0600

    Reverting new storm path changes (it was effecting group expansion) Changing new relic naming convention for transactions

--- init/stormpath.js
+++ init/stormpath.js
@@ -1,28 +1,11 @@
 import path from 'path';
-import config from 'config';
 import { init as stormpath } from 'express-stormpath';
 
 export default function(app) {
   return stormpath(app, {
-    api: true,
-    website: true,
     expandGroups: true,
     registrationView: path.join(__root, 'views/register.jade'),
     loginView: path.join(__root, 'views/login.jade'),
-    // cacheOptions: {
-    //   store: 'redis',
-    //   connection: {
-    //     host: config.get('redis.host'),
-    //     port: config.get('redis.port'),
-    //   },
-    //   options: {
-    //     return_buffers: false,
-    //     auth_pass: config.get('redis.password')
-    //   },
-    //   ttl: 300,
-    //   tti: 300
-    // },
-    getOauthTokenUrl: '/oauth/token',
-    oauthTTL: 5 * 60 // 5 minutes
   });
 }
+
--- middleware/newrelic-transaction-namer.js
+++ middleware/newrelic-transaction-namer.js
@@ -2,9 +2,9 @@ import newrelic from 'newrelic';
 
 export default (req, res, next) => {
   if (req.params.resource) {
-    let name = `/${req.params.resource}`;
+    let name = `${req.params.resource}`;
     if (req.params.id) name += '/:id';
-    newrelic.setTransactionName(name);
+    newrelic.setControllerName(name, req.method);
   }
   next();
 }
