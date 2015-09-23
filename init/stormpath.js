dea9a80bf53c2b39c4991c0cb4e36c13c15790c1
Seth Tippetts <sethtippetts@gmail.com>
2015-09-22 18:14:11 -0600

    Removing custom pages (for now) because CSRF validation was failing on production for some reason

--- init/stormpath.js
+++ init/stormpath.js
@@ -3,9 +3,7 @@ import { init as stormpath } from 'express-stormpath';
 
 export default function(app) {
   return stormpath(app, {
-    expandGroups: true,
-    registrationView: path.join(__root, 'views/register.jade'),
-    loginView: path.join(__root, 'views/login.jade'),
+    expandGroups: true
   });
 }
 
