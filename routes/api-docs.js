ddfc3bae96a020f30873ec9f77a26a5a2fc2b380
Seth Tippetts <sethtippetts@gmail.com>
2015-09-22 20:23:18 -0600

    Slight work on augmentDocs (not really)

--- routes/api-docs.js
+++ routes/api-docs.js
@@ -1,3 +1,14 @@
+'use strict';
+
+import augmentDocs from '../middleware/augment-docs';
+import getModels from '../lib/db';
+
+let api;
+
 export default {
-  get: (req, res) => res.send(req.app.swagger.api)
+  get: (req, res) => {
+    if (api) return res.send(api);
+    getModels()
+      .then(models => res.send(api = augmentDocs(api, models)));
+  }
 }
