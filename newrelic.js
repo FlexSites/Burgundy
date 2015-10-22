b1791ee3e8f6427a323aa82e12b2fc1d23057439
Seth Tippetts <sethtippetts@gmail.com>
2015-10-21 18:12:06 -0600

    Updating newrelic.js to pass linter

--- newrelic.js
+++ newrelic.js
@@ -5,20 +5,23 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
+
   /**
    * Array of application names.
    */
   app_name: ['My Application'],
+
   /**
    * Your New Relic license key.
    */
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
+
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
-  }
+    level: 'info',
+  },
 }
