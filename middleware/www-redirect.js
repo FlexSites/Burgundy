ad01735415964cfd104f621e4aab011b67f09622
Seth Tippetts <sethtippetts@gmail.com>
2015-11-02 19:21:39 -0700

    Forcing HTTPS for all non-local requests

--- middleware/www-redirect.js
+++ middleware/www-redirect.js
@@ -1,11 +1,25 @@
-/**
- * Redirect all requests with a host beginning with www => non-www version
- */
-module.exports = function(){
+import config from 'config';
+
+const PORT = config.get('port');
+const IS_LOCAL = config.get('isDev');
+
+export default () => {
   var isNotApex = /^.*\..*\..*$/;
-  return function(req, res, next){
-    let host = req.hostname;
-    if (host === 'localhost' || isNotApex.test(host)) return next();
-    res.redirect(301, `${req.protocol}://www.${host}${req.url}`);
+  return ({ secure, hostname, protocol, url }, res, next) => {
+
+    let needsSecure = !secure && !IS_LOCAL;
+    let needsWWW = hostname !== 'localhost' && !isNotApex.test(hostname);
+
+    // Redirect to secure everywhere but local
+    if (needsSecure) protocol += 's';
+
+    // Redirect to 'www' if it's Apex
+    if (needsWWW) hostname = `www.${hostname}`;
+
+    // Redirecting
+    if (needsSecure || needsWWW) return res.redirect(301, `${protocol}://${hostname}${url}`);
+
+    // No redirect needed
+    next();
   };
 };
