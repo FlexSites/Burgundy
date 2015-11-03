aed9516138ece178671f24290a018befc9e62443
Seth Tippetts <sethtippetts@gmail.com>
2015-11-02 19:19:06 -0700

    Adding default endpoint to describe models available

---
+++ routes/api/v1.js
@@ -0,0 +1,11 @@
+
+import getModels from '../../lib/db';
+
+export default {
+  get: (req, res, next) => {
+    getModels()
+      .then(Object.keys)
+      .then(res.send.bind(res))
+      .catch(next);
+  }
+}
